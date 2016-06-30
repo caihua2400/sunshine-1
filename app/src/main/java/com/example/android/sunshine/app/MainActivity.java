cabc428de034dcd251bac3831fa63e2ba3396a89
cai hua <caihua2300@gmail.com>
2016-06-30 14:56:58 +1000

    a updation for open a location preference in map. However, it failded

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
@@ -18,12 +21,14 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 
 public class MainActivity extends ActionBarActivity {
+    private final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +47,19 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    public void openPreferedLocationInMap(){
+        SharedPreferences preferences= PreferenceManager.getDefaultSharedPreferences(this);
+        String location=preferences.getString(getString(R.string.pref_location_key),getString(R.string.pref_location_default));
+        Uri geoLocation=Uri.parse("geo:0,0?").buildUpon().appendQueryParameter("q",location).build();
+        Intent intent=new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if(intent.resolveActivity(getPackageManager())!=null){
+             startActivity(intent);
+
+        }else{
+            Log.d(LOG_TAG, "Couldn't call " + location + ", no receiving apps installed!");
+        }
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -54,6 +72,10 @@ public class MainActivity extends ActionBarActivity {
         if (id == R.id.action_settings) {
             Intent intent=new Intent(this,SettingsActivity.class);
             startActivity(intent);
+            return true;
+        }
+        if(id==R.id.action_map){
+            openPreferedLocationInMap();
             return true;
         }
 
