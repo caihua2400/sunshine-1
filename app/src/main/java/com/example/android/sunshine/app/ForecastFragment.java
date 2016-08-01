4ff0afaa5704780d550d1c57519cfa9fc78f097f
cai hua <caihua2300@gmail.com>
2016-08-01 21:48:20 +1000

    For detail commit about loader

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -135,7 +135,19 @@ public class ForecastFragment extends Fragment implements LoaderManager.LoaderCa
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mForecastAdapter);
-
+       listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+           @Override
+           public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+               // CursorAdapter returns a cursor at the correct position for getItem(), or null
+               // if it cannot seek to that position.
+               Cursor cursor=(Cursor) parent.getItemAtPosition(position);
+               if(cursor!=null){
+                   String locationSetting=Utility.getPreferredLocation(getActivity());
+                   Intent intent=new Intent(getActivity(),DetailActivity.class).setData(WeatherContract.WeatherEntry.buildWeatherLocationWithDate(locationSetting,cursor.getLong(COL_WEATHER_DATE)));
+                   startActivity(intent);
+               }
+           }
+       });
 
         return rootView;
     }
